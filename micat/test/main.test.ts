a5f0d91497a1531e7c3520070b1290858e28f99a
stefaneidelloth <eis@isi.fhg.de>
2024-01-17 18:28:35 +0100

    created demo test #28

---
+++ micat/test/main.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+import { app } from '../src/main'
+
+test('app', () => {
+  //TODO: mock function calls to ensure *unit* tests and realistic coverage
+  expect(app).toBeDefined()
+})
