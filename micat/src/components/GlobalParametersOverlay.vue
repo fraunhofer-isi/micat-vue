891840bc3b8c3fd681ff309fbfb5d672d03203cd
Tobias Lorenz <tobias.lorenz@rtl.de>
2024-04-22 18:08:55 +0200

    fix: fixed dependencies and lint issues

--- micat/src/components/GlobalParametersOverlay.vue
+++ micat/src/components/GlobalParametersOverlay.vue
@@ -129,6 +129,10 @@ const entriesAreValid = (entries: Array<GlobalParameterValue>) => {
   // Check if all entries sum up to (almost) 1
   return Math.abs(entries.map((entry) => entry.value).reduce((a, b) => a + b) - 1) < 0.1;
 };
+const reformatCategoryTitle = (categoryName: string) => {
+  let name = categoryName.replace(/([A-Z])/g, ' $1').toLowerCase().trim();
+  return name[0].toUpperCase() + name.slice(1);
+};
 const getCategoryTitle = (categoryName: string) => {
   switch (categoryName) {
     case 'FuelSplitCoefficient':
@@ -136,8 +140,7 @@ const getCategoryTitle = (categoryName: string) => {
     case 'EnergyPrice':
       return 'Energy prices';
     default:
-      let name = categoryName.replace(/([A-Z])/g, ' $1').toLowerCase().trim();
-      return name[0].toUpperCase() + name.slice(1);
+      return reformatCategoryTitle(categoryName);
   }
 };
 const getFactorTitle = (factor: string) => {
@@ -158,18 +161,18 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
 </script>
 
 <template>
-  <div role="status" v-if="loading" class="text-center p-5">
+  <div role="status" v-if="loading" class="p-5 text-center">
       <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-orange-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
       </svg>
       <span class="sr-only">Loading...</span>
   </div>
-  <div v-else class="max-w-screen-xl mx-auto pt-5 pb-10">
-    <div class="flex items-center gap-2 justify-between pb-2">
+  <div v-else class="max-w-screen-xl pt-5 pb-10 mx-auto">
+    <div class="flex items-center justify-between gap-2 pb-2">
       <a href="#" @click="$emit('close')" class="text-sm text-orange-700 dark:text-orange-300">back to the entries</a>
       <button
-        class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 pl-3 pr-4 rounded-full uppercase text-xs mr-3"
+        class="py-2 pl-3 pr-4 mr-3 text-xs font-bold text-white uppercase bg-gray-500 rounded-full hover:bg-gray-600"
         @click="reset()"
         :disabled="loading"
       >
@@ -177,13 +180,13 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
         Reset to defaults
       </button>
     </div>
-    <div class="rounded-3xl border border-gray-300 my-3 relative bg-white">
+    <div class="relative my-3 bg-white border border-gray-300 rounded-3xl">
       <div @click="$emit('close')"
            class="bg-white dark:bg-blue-950 rounded-full p-1 absolute top-[-20px] right-[-10px] cursor-pointer">
         <XCircleIcon class="text-orange-700 dark:text-orange-300 h-9 w-9"></XCircleIcon>
       </div>
       <div class="flex">
-        <div class="bg-orange-600 rounded-l-3xl self-stretch">
+        <div class="self-stretch bg-orange-600 rounded-l-3xl">
           <div
             class="flex items-center px-5 cursor-pointer"
             @click="selectCategory(categoryName)"
@@ -203,15 +206,15 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
               <span class="font-bold">{{ getCategoryTitle(categoryName) }}</span>
               <InformationCircleIcon
                 @click="openModal(categoryName)"
-                class="h-6 w-6 ml-2 cursor-pointer inline"
+                class="inline w-6 h-6 ml-2 cursor-pointer"
               ></InformationCircleIcon>
             </div>
           </div>
         </div>
         <div class="flex" v-if="activeCategory">
-          <div class="bg-orange-500 self-start" v-if="Object.keys(globalParameters[activeCategory]).length > 1">
+          <div class="self-start bg-orange-500" v-if="Object.keys(globalParameters[activeCategory]).length > 1">
             <div
-              class="flex items-center px-3 py-3 cursor-pointer text-sm"
+              class="flex items-center px-3 py-3 text-sm cursor-pointer"
               @click="activeSubsector = Number(subsector)"
               :class="{
                 'text-white': activeSubsector !== Number(subsector),
@@ -223,8 +226,8 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
               v-for="(subsector, i) in Object.keys(globalParameters[activeCategory])"
               v-bind:key="`global-parameters-subcategory-${i}`"
             >
-              <span class="font-bold mr-8 grow whitespace-nowrap">{{ subsectorMapping[Number(subsector)] }}</span>
-              <CheckIcon v-if="activeSubsector === Number(subsector)" class="h-5 w-5"></CheckIcon>
+              <span class="mr-8 font-bold grow whitespace-nowrap">{{ subsectorMapping[Number(subsector)] }}</span>
+              <CheckIcon v-if="activeSubsector === Number(subsector)" class="w-5 h-5"></CheckIcon>
             </div>
           </div>
           <div class="px-5 py-3">
@@ -233,25 +236,25 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
               v-bind:key="`global-parameters-years-${yearOrFactor}`"
               class="block rounded-xl bg-white border border-orange-600 m-5 max-w-[450px]">
               <div
-                class="bg-orange-600 rounded-t-xl text-white px-4 py-2">
+                class="px-4 py-2 text-white bg-orange-600 rounded-t-xl">
                 {{ activeCategory === 'MonetisationFactors' ? getFactorTitle(monetisationFactorMapping[Number(yearOrFactor)]) : yearOrFactor }}
                 <InformationCircleIcon
                   v-if="activeCategory === 'MonetisationFactors'"
                   @click="openModal(getCleanedMonetisationFactorName(monetisationFactorMapping[Number(yearOrFactor)]))"
-                  class="h-6 w-6 ml-2 cursor-pointer inline"
+                  class="inline w-6 h-6 ml-2 cursor-pointer"
                 ></InformationCircleIcon>
               </div>
               <div class="p-4">
                 <div
                   v-for="(entry, i) in entries"
                   v-bind:key="`global-parameters-years-${yearOrFactor}-${i}`"
-                  class="grid gap-2 py-1 items-center"
+                  class="grid items-center gap-2 py-1"
                   :class="{
                     'grid-cols-3': entries.length > 1,
                     'grid-cols-2': entries.length == 1,
                   }"
                 >
-                  <div class="text-orange-600 font-bold text-xs" v-if="entries.length > 1">{{ activeCategory === 'MonetisationFactors' ? entry.key : carrierMapping[entry.key] }}</div>
+                  <div class="text-xs font-bold text-orange-600" v-if="entries.length > 1">{{ activeCategory === 'MonetisationFactors' ? entry.key : carrierMapping[entry.key] }}</div>
                   <div>
                     <input
                       :id="`global-parameters-years-${yearOrFactor}-${i}-range`"
@@ -274,7 +277,7 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
                 </div>
                 <div
                   v-if="activeCategory !== 'MonetisationFactors' && !entriesAreValid(entries)"
-                  class="flex p-4 mt-5 text-yellow-800 border-t-4 border-yellow-300 bg-yellow-50 rounded-2xl text-sm"
+                  class="flex p-4 mt-5 text-sm text-yellow-800 border-t-4 border-yellow-300 bg-yellow-50 rounded-2xl"
                   role="alert"
                 >
                   <ExclamationCircleIcon class="h-7 w-7"></ExclamationCircleIcon>
