bfcd5d27c575410fd58d66c9d3dffa3fd6e0bd70
Tobias Lorenz <tobias.lorenz@rtl.de>
2024-09-02 17:23:28 +0200

    fix: fixed linting issues

--- micat/src/components/ResultsOverlay.vue
+++ micat/src/components/ResultsOverlay.vue
@@ -643,12 +643,14 @@ const {openModal} = inject<ModalInjectInterface>('modal') || defaultModalInject
           </div>
           <div class="px-10 py-5">
             <!-- @vue-ignore -->
+            <!-- eslint-disable --> 
             <Line
               class="w-full max-w-[100%]"
               id="chart"
               :options="chartOptions"
               :data="chartData"
             />
+            <!-- eslint-enable -->
           </div>
         </div>
         <div v-else-if="activeCategory === 'aggregation'" class="w-full max-w-full p-7">
